fe8ffe0841aabbdb4885776bd5a57568d50b0b7d
Hakiimdaud <cabdihakiim395@gmail.com>
2025-07-19 21:52:34 -0700

    Clock_Time

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const hour = document.querySelector("#hour");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+const AmPm = document.querySelector("#AmPm");
+
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+
+function clock_Time() {
+    const dateTime = new Date();
+    //    Hours
+    dateTime.getHours() % 12 < 10 ? hour.innerHTML = `0${dateTime.getHours() % 12} :`
+        : hour.innerHTML = dateTime.getHours() % 12 + " :";
+    // Minutes
+    dateTime.getMinutes() < 10 ? minutes.innerHTML = `0${dateTime.getMinutes()} :`
+        : minutes.innerHTML = dateTime.getMinutes() + " :";
+    // Seconds
+    dateTime.getSeconds() < 10 ? seconds.innerHTML = `0${dateTime.getSeconds()}`
+        : seconds.innerHTML = dateTime.getSeconds();
+    // AmOrPam
+    dateTime.getHours() > 12 ? amPm.innerHTML = "PM" : amPm.innerHTML = "AM"
+
+}
+
+
+setInterval(clock_Time);
